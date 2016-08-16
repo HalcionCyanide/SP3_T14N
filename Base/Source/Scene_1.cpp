ff72b9891f6a7dc7109437be59b726abee553232
sekheng <leesekheng@yahoo.com>
2016-08-16 17:47:33 +0800

    Trying to fix the SekHeng user

--- Base/Source/Scene_1.cpp
+++ Base/Source/Scene_1.cpp
@@ -87,6 +87,7 @@ void Scene_1::Init()
     theMap->LoadMap("Image//MapTest.csv", m_heightMap, TerrainScale, bunchOfRenderingStuff);
     for (auto it : bunchOfRenderingStuff)
         delete it;
+    //Hello Sek Heng here
 }
 
 void Scene_1::Update(float dt)
