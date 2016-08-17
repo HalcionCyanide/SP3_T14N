caffcaa260db98c91e61ddaadf8d0779c0157cdc
sekheng <150629Z@mymail.nyp.edu.sg>
2016-08-17 20:17:11 +0800

    in-progress command structure.

--- Base/Source/Commands.cpp
+++ Base/Source/Commands.cpp
@@ -1,9 +1,46 @@
 #include "Commands.h"
 
 unsigned char ForwardCommand::theKey = 'w';
+unsigned char BackCommand::theKey = 's';
+unsigned char RightCommand::theKey = 'a';
+unsigned char LeftCommand::theKey = 'd';
+
+ForwardCommand::ForwardCommand()
+{
+
+}
 
 ForwardCommand::~ForwardCommand()
 {
 
 }
 
+BackCommand::BackCommand()
+{
+
+}
+
+BackCommand::~BackCommand()
+{
+
+}
+
+RightCommand::RightCommand()
+{
+
+}
+
+RightCommand::~RightCommand()
+{
+
+}
+
+LeftCommand::LeftCommand()
+{
+
+}
+
+LeftCommand::~LeftCommand()
+{
+
+}
