df6499f18ecaf2f1b648ac94cac95be245b9c61e
sekheng <150629Z@mymail.nyp.edu.sg>
2016-08-20 15:49:13 +0800

    On the way to code a much more object oriented commands but failed so bad.

--- Base/Source/Commands.cpp
+++ Base/Source/Commands.cpp
@@ -1,12 +1,26 @@
 #include "Commands.h"
 
-unsigned char ForwardCommand::theKey = 'W';
-unsigned char BackCommand::theKey = 'S';
-unsigned char RightCommand::theKey = 'A';
-unsigned char LeftCommand::theKey = 'D';
+std::vector<Command*> Command::All_Command_Type;
+
+//unsigned char ForwardCommand::theKey = 'W';
+//unsigned char BackCommand::theKey = 'S';
+//unsigned char RightCommand::theKey = 'A';
+//unsigned char LeftCommand::theKey = 'D';
+
+Command::Command()
+{
+    theKey = NULL;
+}
+
+Command::~Command()
+{
+    
+}
 
 ForwardCommand::ForwardCommand()
 {
+    theKey = 'W';
+    All_Command_Type.push_back(this);
 }
 
 ForwardCommand::~ForwardCommand()
@@ -15,6 +29,8 @@ ForwardCommand::~ForwardCommand()
 
 BackCommand::BackCommand()
 {
+    theKey = 'S';
+    All_Command_Type.push_back(this);
 }
 
 BackCommand::~BackCommand()
@@ -23,6 +39,8 @@ BackCommand::~BackCommand()
 
 RightCommand::RightCommand()
 {
+    theKey = 'D';
+    All_Command_Type.push_back(this);
 }
 
 RightCommand::~RightCommand()
--- Base/Source/Commands.h
+++ Base/Source/Commands.h
@@ -1,35 +1,42 @@
-#pragma once
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <vector>
 
 struct Command
 {
-    Command() {};
-    virtual ~Command() {};
+    static std::vector<Command*> All_Command_Type;
+    Command();
+    virtual ~Command();
+    unsigned char theKey;
 };
 
 struct ForwardCommand : public Command
 {
     ForwardCommand();
     virtual ~ForwardCommand();
-    static unsigned char theKey;
+    //static unsigned char theKey;
 };
 
 struct BackCommand : public Command
 {
     BackCommand();
     virtual ~BackCommand();
-    static unsigned char theKey;
+    //static unsigned char theKey;
 };
 
 struct RightCommand : public Command
 {
     RightCommand();
     virtual ~RightCommand();
-    static unsigned char theKey;
+    //static unsigned char theKey;
 };
 
 struct LeftCommand : public Command
 {
     LeftCommand();
     virtual ~LeftCommand();
-    static unsigned char theKey;
+    //static unsigned char theKey;
 };
+
+#endif
