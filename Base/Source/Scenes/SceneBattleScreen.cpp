c96bcda6b6505d17e9f2ef34dd9ad23d9ed21288
idealizinc <idealizinc@gmail.com>
2016-08-26 15:14:05 +0800

    Sek Heng does these stuff.

--- Base/Source/Scenes/SceneBattleScreen.cpp
+++ Base/Source/Scenes/SceneBattleScreen.cpp
@@ -168,16 +168,20 @@ void SceneBattleScreen::RenderPassMain()
 	ss.precision(3);
 	SceneGraphics->RenderTextOnScreen("text", ss.str(), Color(0, 1, 0), 25, 25, 50);
 
-	ss.str("");
-	ss << "EnemyMWaves" << Scene_System::accessing().BSys->CurrentEnemy->MaxEnemyWave;
-	ss.precision(3);
-	SceneGraphics->RenderTextOnScreen("text", ss.str(), Color(0, 1, 0), 25, 25, 75);
+	if (Scene_System::accessing().BSys->CurrentEnemy)
+	{
+		ss.str("");
+		ss << "EnemyMWaves" << Scene_System::accessing().BSys->CurrentEnemy->MaxEnemyWave;
+		ss.precision(3);
+		SceneGraphics->RenderTextOnScreen("text", ss.str(), Color(0, 1, 0), 25, 25, 75);
 
-	ss.str("");
-	ss << "EnemyCWaves" << Scene_System::accessing().BSys->CurrentEnemy->CurrentEnemyWave;
-	ss.precision(3);
-	SceneGraphics->RenderTextOnScreen("text", ss.str(), Color(0, 1, 0), 25, 25, 100);
+		ss.str("");
+		ss << "EnemyCWaves" << Scene_System::accessing().BSys->CurrentEnemy->CurrentEnemyWave;
+		ss.precision(3);
+		SceneGraphics->RenderTextOnScreen("text", ss.str(), Color(0, 1, 0), 25, 25, 100);
 
+	}
+	
 	SceneGraphics->SetHUD(false);
 }
 
--- Base/Source/Scenes/SceneFreeField.cpp
+++ Base/Source/Scenes/SceneFreeField.cpp
@@ -114,11 +114,11 @@ void SceneFreeField::Update(float dt)
 		if (Math::RandIntMinMax(0, MaxEncounterRate - CurrentEncounterRateBoost) < MaxEncounterRate * EncounterRatio)
 		{
 			CurrentEncounterRateBoost = 0;
-			/*int i = 0;
+		/*	int i = 0;
 			for (std::map<std::string, Enemy*>::iterator it = Scene_System::accessing().EnemyData.begin(); it != Scene_System::accessing().EnemyData.end(); ++it)
 			{
-				int size = Scene_System::accessing().EnemyData.size();
-				int choice = Math::RandIntMinMax(0, Scene_System::accessing().EnemyData.size());
+				int size = Scene_System::accessing().EnemyData.size() - 1;
+				int choice = Math::RandIntMinMax(0, size);
 				if (i == choice)
 				{
 					Scene_System::accessing().BSys->SetEnemy(*it->second);
@@ -126,8 +126,12 @@ void SceneFreeField::Update(float dt)
 				}
 				i++;
 			}*/
-			std::map<std::string, Enemy*>::iterator it = Scene_System::accessing().EnemyData.begin();
+			std::ostringstream ss;
+			ss << Math::RandIntMinMax(1, Scene_System::accessing().EnemyData.size());
+			std::map<std::string, Enemy*>::iterator it = Scene_System::accessing().EnemyData.find(ss.str());
 			Scene_System::accessing().BSys->SetEnemy(*it->second);
+			//std::map<std::string, Enemy*>::iterator it = Scene_System::accessing().EnemyData.begin();
+			//Scene_System::accessing().BSys->SetEnemy(*it->second);
 			Scene_System::accessing().SwitchScene(SceneBattleScreen::id_);
 		}
 	}
