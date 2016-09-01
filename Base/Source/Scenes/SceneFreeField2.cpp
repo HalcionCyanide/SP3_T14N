1b71e727344d05b5b73cde8be941d9aa943c0726
Qing ping <qingping1998@gmail.com>
2016-09-01 12:18:22 +0800

    no message

--- Base/Source/Scenes/SceneFreeField2.cpp
+++ Base/Source/Scenes/SceneFreeField2.cpp
@@ -215,9 +215,12 @@ void SceneFreeField2::NPC_chat(float dt)
 											{
 												if (test->getActive())
 												{
-													if (test->qStages.at(it5->second - 1)->getComplete())
+													if (it5->second > 0)
 													{
-														imdone = true;
+														if (test->qStages.at(it5->second - 1)->getComplete())
+														{
+															imdone = true;
+														}
 													}
 												}
 											}
