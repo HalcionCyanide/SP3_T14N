773afb4e7ef8bbf229d8972004bcee003a826194
Qing ping <qingping1998@gmail.com>
2016-09-02 10:57:48 +0800

    no message

--- Base/Source/Systems/InputManager.cpp
+++ Base/Source/Systems/InputManager.cpp
@@ -27,18 +27,18 @@ void InputManager::HandleUserInput()
         }
     }
     // hardcoded inputs. A need for change. So i really need everyone's help
-	//for (unsigned i = 0; i < 256; ++i)
-	//{
-	//	// Check Keypress
-	//	if (Application::IsKeyPressed(i) && cIM_Keys[i] == false)
-	//	{
-	//		cIM_Keys[i] = true;
-	//	}
-	//	else if (!Application::IsKeyPressed(i) && cIM_Keys[i] == true)
-	//	{
-	//		cIM_Keys[i] = false;
-	//	}
-	//}
+	for (unsigned i = 0; i < 256; ++i)
+	{
+		// Check Keypress
+		if (Application::IsKeyPressed(i) && cIM_Keys[i] == false)
+		{
+			cIM_Keys[i] = true;
+		}
+		else if (!Application::IsKeyPressed(i) && cIM_Keys[i] == true)
+		{
+			cIM_Keys[i] = false;
+		}
+	}
 }
 
 bool InputManager::GetKeyValue(char c)
